130c294cfe6c4cbd3efb63401603b83f2f8ce8ea
Ashish Tomar <ashishtomar135@gmail.com>
2023-12-18 20:20:24 +0530

    formatted document

--- src/components/Attendance/AttendanceGrid.js
+++ src/components/Attendance/AttendanceGrid.js
@@ -1,35 +1,38 @@
 // AttendanceGrid.js
 
-import React from 'react';
+import React from "react";
 
 const AttendanceGrid = ({ teachers, onAttendanceChange }) => {
-    //const [selectedValue, setSelectedValue] = useState('');
+  //const [selectedValue, setSelectedValue] = useState('');
   return (
     <>
-    <table>
-      <thead>
-        <tr>
-          <th>Teacher</th>
-          <th>Attendance</th>
-        </tr>
-      </thead>
-      <tbody>
-        {teachers.length > 0 && teachers.map((teacher) => (
-          <tr key={teacher.name}>
-            <td>{teacher.name}</td>
-            <td>
-              <select
-                value={teacher.attendance}
-                onChange={(e) => onAttendanceChange(teacher.name, e.target.value)}
-              >
-                <option value="Present">Present</option>
-                <option value="Absent">Absent</option>
-              </select>
-            </td>
+      <table>
+        <thead>
+          <tr>
+            <th>Teacher</th>
+            <th>Attendance</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {teachers.length > 0 &&
+            teachers.map((teacher) => (
+              <tr key={teacher.name}>
+                <td>{teacher.name}</td>
+                <td>
+                  <select
+                    value={teacher.attendance}
+                    onChange={(e) =>
+                      onAttendanceChange(teacher.name, e.target.value)
+                    }
+                  >
+                    <option value="Present">Present</option>
+                    <option value="Absent">Absent</option>
+                  </select>
+                </td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
     </>
   );
 };
--- src/components/Schedule/ScheduleGrid.js
+++ src/components/Schedule/ScheduleGrid.js
@@ -1,6 +1,6 @@
 // ScheduleGrid.js
 
-import React from 'react';
+import React from "react";
 
 const ScheduleGrid = ({ schedule }) => {
   return (
