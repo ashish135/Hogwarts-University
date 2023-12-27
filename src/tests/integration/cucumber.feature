875e418030ede79c61a2bfcdfb3dd236d5273e02
Ashish Tomar <ashishtomar135@gmail.com>
2023-12-27 16:27:19 +0530

    working on cucumber

--- src/tests/integration/cucumber.feature
+++ src/tests/integration/cucumber.feature
@@ -1,7 +1,23 @@
-Feature: Teacher Attendance and Student Schedule
+Feature: Attendance Grid Functionality
 
-  Scenario: Update teacher attendance and verify student schedule
-    Given the teacher attendance grid is displayed
-    When I change the attendance of "Professor Dumbledore" to "Absent"
-    Then the current schedule should be updated accordingly
-    And "Harry Potter" should be assigned a new teacher
+  Scenario: Render the table header
+    Given the attendance grid is displayed
+    Then the table header should contain "Teacher" and "Attendance"
+
+  Scenario: Render a row for each teacher
+    Given the attendance grid is displayed with the following teachers:
+      | Teacher               | Attendance |
+      | Professor Dumbledore  | Present    |
+      | Minerva McGonagall    | Absent     |
+    Then each teacher should have a corresponding row with their name and attendance status
+
+  Scenario: Change attendance and verify onAttendanceChange callback
+    Given the attendance grid is displayed with the following teachers:
+      | Teacher               | Attendance |
+      | Professor Dumbledore  | Present    |
+      | Minerva McGonagall    | Absent     |
+    When the attendance for "Professor Dumbledore" is changed to "Absent"
+    Then the onAttendanceChange callback should be called with arguments "Professor Dumbledore" and "Absent"
+
+    When the attendance for "Minerva McGonagall" is changed to "Present"
+    Then the onAttendanceChange callback should be called with arguments "Minerva McGonagall" and "Present"
--- src/tests/integration/cucumber.steps.js
+++ src/tests/integration/cucumber.steps.js
@@ -1,24 +1,50 @@
-// cucumber.steps.js
+// attendance.steps.js
+import React from 'react';
+import { Given, Then, When } from '@cucumber/cucumber';
+import { render, fireEvent } from '@testing-library/react';
+import AttendanceGrid from '../../components/Attendance/AttendanceGrid';
 
-import { Given, When, Then } from 'cucumber';
-import { render, fireEvent, screen } from '@testing-library/react';
-import App from '../../App';
+const mockTeachers = [
+  { name: 'Professor Dumbledore', attendance: 'Present' },
+  { name: 'Minerva McGonagall', attendance: 'Absent' },
+];
 
-let appRender;
+let component;
+let onAttendanceChangeMock;
 
-Given('the teacher attendance grid is displayed', () => {
-  appRender = render(<App />);
+Given('the attendance grid is displayed', () => {
+  // You can set up the initial rendering of the AttendanceGrid here
+  component = render(<AttendanceGrid teachers={[]} onAttendanceChange={() => {}} />);
 });
 
-When('I change the attendance of {string} to {string}', (teacher, attendance) => {
-  const selectTeacher = screen.getByLabelText(`Attendance for ${teacher}`);
-  fireEvent.change(selectTeacher, { target: { value: attendance } });
+Given('the attendance grid is displayed with the following teachers:', (dataTable) => {
+  // You can set up the initial rendering of the AttendanceGrid with mock data here
+  const teachers = dataTable.hashes().map(({ Teacher, Attendance }) => ({ name: Teacher, attendance: Attendance }));
+  component = render(<AttendanceGrid teachers={teachers} onAttendanceChange={() => {}} />);
 });
 
-Then('the current schedule should be updated accordingly', () => {
-  // Add assertions to verify the updated schedule
+When('the attendance for {string} is changed to {string}', (teacher, attendance) => {
+  // Change attendance for the specified teacher
+  const selectElement = component.getByTestId(`attendance-select-${teacher.replace(' ', '-')}`);
+  fireEvent.change(selectElement, { target: { value: attendance } });
 });
 
-Then('{string} should be assigned a new teacher', (student) => {
-  // Add assertions to verify that the student is assigned a new teacher
+Then('the table header should contain {string} and {string}', (header1, header2) => {
+  expect(component.getByText(header1)).toBeInTheDocument();
+  expect(component.getByText(header2)).toBeInTheDocument();
 });
+
+Then('each teacher should have a corresponding row with their name and attendance status', () => {
+  // Add assertions to check if each teacher has a corresponding row with name and attendance status
+  mockTeachers.forEach(({ name, attendance }) => {
+    expect(component.getByText(name)).toBeInTheDocument();
+    expect(component.getByText(attendance)).toBeInTheDocument();
+  });
+});
+
+Then('the onAttendanceChange callback should be called with arguments {string} and {string}', (teacher, attendance) => {
+  expect(onAttendanceChangeMock).toHaveBeenCalledWith(teacher, attendance);
+});
+
+// You may also need a Before hook to set up common things before each scenario
+// e.g., Before(() => { /* set up common things */ });
